bb8b0857ef5765ec9726e9d7ed6cc75f09a88563
Polly <apollinaria9203@yandex.by>
2022-01-30 18:50:58 +0300

    Bag fix

--- BigBookWeb/Controllers/CategoryController.cs
+++ BigBookWeb/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BigBookWeb.Data;
+using BigBookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BigBookWeb.Controllers
@@ -22,6 +23,16 @@ namespace BigBookWeb.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
