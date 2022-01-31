abd5324248bb129abd8ee5f5065555b277314524
Polly <apollinaria9203@yandex.by>
2022-01-31 13:02:06 +0300

    Added Edit action

--- BigBookWeb/Controllers/CategoryController.cs
+++ BigBookWeb/Controllers/CategoryController.cs
@@ -43,5 +43,38 @@ namespace BigBookWeb.Controllers
 
             return View(obj);
         }
+
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            var category = _db.Categories.Find(id);
+
+            if (id == null || id == 0 || category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Name cannot exactly match the DisplayOrder");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
     }
 }
